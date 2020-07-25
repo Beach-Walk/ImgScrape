1e2b1c44b068a444ad7cced9d10658ab72dfbc22
Beach-Walk <1688031+Beach-Walk@users.noreply.github.com>
2020-07-24 23:42:09 -0400

    Very Clean Update
    -removed some old code
    -added menu loop for multiple scrapes
    -now headless
    -now detects if LOAD MORE button is present and runs accordingly
    -improved username finding, doesnt use url and instead uses xpath
    -added fix if usernames are not found, will name folder random numbers
    -cleaned up some text prompts

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from selenium import webdriver;
+from selenium.webdriver.chrome.options import Options
 import time;
 import random;
 import urllib;
@@ -6,75 +7,45 @@ import os;
 import urllib.request
 from bs4 import BeautifulSoup;
 from vsco import *
+menu=True
+while menu is True:
+    chrome_path =r"drivers\chrome\chromedriver.exe" #driver needs to be the same version as your chrome version
+    options = Options()
+    options.headless=True
+    driver = webdriver.Chrome(chrome_path,chrome_options=options)
 
-chrome_path =r"drivers\chrome\chromedriver.exe" #driver needs to be the same version as your chrome version
-profileLink = input("enter a vsco profile url")
-driver = webdriver.Chrome(chrome_path)
-driver.get(profileLink)
-print("trying: ",profileLink," ...")
-option = 0
-if option ==1:
-    profileName = "test23"
-    saveDir = "img/" + profileName
+    profileLink = input("Enter a VSCO profile url...\n")
+    driver.get(profileLink)
+    print("Trying: ",profileLink," ...")
 
+    uName= get_vsco_un(driver)
+    saveDir="img/"+uName
 
-    # Get scroll height
-    last_height = driver.execute_script("return document.body.scrollHeight")
-
-    while True:
-        # Scroll down to bottom
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # Wait to load page
-        time.sleep(random.uniform(0.5, 1))
-
-        # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
-
-
-    time.sleep(random.uniform(3, 10))  # time to wait
-
-    htmlSource=driver.page_source#get html code from selenium loaded site
-
-    soup= BeautifulSoup(htmlSource,"html.parser")#input html code from selenium to beautiful soup
-
-    galleryItems = soup.find_all("figure", {"class": "gallery-item"})
-
-   # imgTagsInGallery = galleryItems.find_all('img')
-
-    finalImgUrls = []
-
-    for imgs in galleryItems:
-        if imgs.find("img") != None:  # make sure it has the set of img res (x1 x2 x3)
-            singleImg = imgs.find("img")
-
-            string = singleImg.get("src")  # set it to a var for manipulation
-
-            finalImgUrls.append(str(string))
-
-if option ==0:
-    saveDir= get_vsco_un(profileLink)
     finalImgUrls= get_vsco_imgs(profileLink,driver)
 
+    count = 0;
+    print("Found "+str(len(finalImgUrls))+" images!")
 
-count = 0;
-print("Found ",len(finalImgUrls)," images!")
+    if not os.path.exists(saveDir):#create folder with the name of the vcso account.
+        os.mkdir(saveDir)
+        print("Directory '"+str(saveDir)+"' Created ")
+    else:
+        print("Directory '"+str(saveDir)+"' already exists files may be overwritten...")
+        time.sleep(3)
 
-if not os.path.exists(saveDir):#create folder with the name of the vcso account.
-    os.mkdir(saveDir)
-    print("Directory " , saveDir ,  " Created ")
-else:
-    print("Directory " , saveDir ,  " already exists")
+    for imgs in finalImgUrls:
+        print("Saving: "+imgs,end='')
+        imagefile = open(saveDir + "/" + str(count)+".jpg", "wb")
+        imagefile.write(urllib.request.urlopen(str(imgs)).read())
+        imagefile.close()
+        print(" DONE!")
+        count=count+1
 
-for imgs in finalImgUrls:
-    print("Saving: "+imgs)
-    imagefile = open(saveDir + "/" + str(count)+".jpg", "wb")
-    imagefile.write(urllib.request.urlopen(str(imgs)).read())
-    imagefile.close()
-    print("DONE!")
-    count=count+1
-
-print("IMAGE SCRAPE COMPLETE")
+    print("IMAGE SCRAPE COMPLETE")
+    driver.quit()
+    print("Driver closed\n=====")
+    print("0. Exit")
+    print("1. Scrape Another")
+    userOption = int(input("\n"))
+    if userOption == 0 or userOption > 1:
+        menu=False
--- vsco.py
+++ vsco.py
@@ -6,37 +6,46 @@ import os;
 import urllib.request
 from bs4 import BeautifulSoup;
 
-def get_vsco_un(profileLink):
+def get_vsco_un(driver):#find username with xpath, if it cant find choose random number
+    findUName = driver.find_elements_by_xpath("/html/body/div/div/main/div/div[1]/div[2]/h3")
+    if len(findUName)>0:
+        uName = driver.find_elements_by_xpath("/html/body/div/div/main/div/div[1]/div[2]/h3")[0].text#get username from text under pfp
+        return uName
+    else:
+        randGalName = str("gallery_"+str(int(random.uniform(0, 10000000))))# generate random number
+        print("Username element not found, using '"+randGalName+"' for folder name...")
+        return randGalName
 
-    profileName = profileLink[profileLink.rindex("/")+1:]  # cut out the username from the link
-    saveDir = "img/" + profileName
-    print(saveDir)
-    return saveDir
+def get_vsco_imgs(profileLink,driver): #get images and return an array/list of the urls
 
-def get_vsco_imgs(profileLink,driver):
+    time.sleep(random.uniform(2, 5))# time to wait
 
-    time.sleep(random.uniform(3, 10))# time to wait
+    findLoadButton = driver.find_elements_by_xpath("/html/body/div/div/main/div/div[5]/section/div[2]/button")
 
-    loadMoreButton = driver.find_elements_by_xpath("/html/body/div/div/main/div/div[5]/section/div[2]/button")[0]#click load more button
-    loadMoreButton.click()
+    if len(findLoadButton)>0:
+        print("LOAD MORE Button Found...")
+        loadMoreButton = driver.find_elements_by_xpath("/html/body/div/div/main/div/div[5]/section/div[2]/button")[0]  # click load more button
+        loadMoreButton.click()
 
-    time.sleep(random.uniform(3, 10))# time to wait
+        time.sleep(random.uniform(2, 5))# time to wait
 
-    # Get scroll height
-    last_height = driver.execute_script("return document.body.scrollHeight")
+        # Get scroll height
+        last_height = driver.execute_script("return document.body.scrollHeight")
 
-    while True:
-        # Scroll down to bottom
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        while True:
+            # Scroll down to bottom
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-        # Wait to load page
-        time.sleep(random.uniform(0.5, 1))
+            # Wait to load page
+            time.sleep(random.uniform(0.5, 1))
 
-        # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
+            # Calculate new scroll height and compare with last scroll height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+    else:
+        print("No LOAD MORE Button Found... Skipping Scroll...")
 
     htmlSource=driver.page_source#get html code from selenium loaded site
 
